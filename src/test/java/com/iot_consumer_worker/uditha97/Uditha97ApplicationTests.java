1d99ab86ccdd373bae2d60359355880a3f95f3a1
udithanayanajith <udithanayanajith97@gmail.com>
2025-09-10 12:53:23 +0530

    initial commit

---
+++ src/test/java/com/iot_consumer_worker/uditha97/Uditha97ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot_consumer_worker.uditha97;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uditha97ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
